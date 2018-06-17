3fc0448364527e6110e2d1b0db5ff498fae7f195
MSe1969 <mse1969@posteo.de>
2018-06-17 19:28:35 +0200

    Initial repo for branch lineage-15.1

---
+++ build_oneplus3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Initiate environment
+source build/envsetup.sh
+
+# CCache
+# ------
+# uncomment the below line to set own cache 
+# directory (default is ~/.ccache)
+export USE_CCACHE=1
+#export CCACHE_DIR=~/android/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 48G
+
+# un-comment below line, if you want to build with root baked in
+# export WITH_SU=true
+
+# microG build
+export RELEASE_TYPE=UNOFFICIAL-microG
+
+# Normalize build metadata
+export KBUILD_BUILD_USER=android
+export KBUILD_BUILD_HOST=localhost
+
+# start build
+brunch oneplus3
+
