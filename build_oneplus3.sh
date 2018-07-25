53569912a3bc9c65ed25e5c311b7273fe0f6ad54
MSe1969 <mse1969@posteo.de>
2018-07-25 23:09:01 +0200

    Build script for signed build
    Change-Id: Icaf24385505afb42c99cb4b259cf60fccce18bbf

--- build_oneplus3.sh
+++ build_oneplus3.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check parameters
+case "$1" in
+  test) TESTKEY=true
+    ;;
+  sign) TESTKEY=false
+    ;;
+  *) echo "usage: build_oneplus3 test|sign"
+     echo "-------------------------------"
+     echo "test - build with testkeys (insecure, but compatible)"
+     echo "sign - create a signed build"
+     exit
+    ;;   
+esac
+
 # Initiate environment
 source build/envsetup.sh
 
@@ -22,5 +36,32 @@ export KBUILD_BUILD_USER=android
 export KBUILD_BUILD_HOST=localhost
 
 # start build
-brunch oneplus3
+if [ "$TESTKEY" = true ] ; then
+    brunch oneplus3
+else
+    # Variables
+    ROMDATE=`date +%Y%m%d`
+    ROMNAME="lineage-15.1-$ROMDATE-$RELEASE_TYPE-oneplus3-signed.zip"
+    # Dist target
+    rm -rf $OUT_DIR_COMMON_BASE/lin-15.1/dist
+    breakfast oneplus3
+    mka target-files-package dist
+    # Sign the apks
+    croot
+    ./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
+        out/dist/*-target_files-*.zip \
+        out/dist/op3-signed-target_files.zip
+    # Create & sign OTA ZIP
+    ./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
+        --block --backup=true \
+        out/dist/op3-signed-target_files.zip \
+        out/dist/op3-signed-ota_update.zip
+    # Sign OTA ZIP
+#    ./build/tools/releasetools/sign_zip.py -k ~/.android-certs/releasekey \
+#        out/dist/op3-signed-ota_update.zip \
+#        out/dist/op3-signed-ota_update-zipsgn.zip
+    # 'Bacon' target
+    ln -f out/dist/op3-signed-ota_update.zip out/target/product/oneplus3/$ROMNAME
+    md5sum out/target/product/oneplus3/$ROMNAME | sed "s|out/target/product/oneplus3/||" > out/target/product/oneplus3/$ROMNAME.md5sum
+fi
 
