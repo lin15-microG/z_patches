bc98532610fdc20eeffc6aed5df73aafa55cd8b5
MSe1969 <mse1969@posteo.de>
2018-11-03 16:30:05 +0100

    Build regular lineage-15.1 also optionally with own keys
    Change-Id: I3beab68510d8ac228ead92c3ff01837a67990330

--- build_oneplus3.sh
+++ build_oneplus3.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check parameters
+case "$1" in
+  test) TESTKEY=true
+    ;;
+  sign) TESTKEY=false
+    ;;
+  *) echo "usage: build_oneplus3 test|sign"
+     echo "-------------------------------"
+     echo "test - build with testkeys (insecure, but compatible)"
+     echo "sign - create a signed build"
+     exit
+    ;;
+esac
+
 # Initiate environment
 source build/envsetup.sh
 
@@ -18,6 +32,20 @@ prebuilts/misc/linux-x86/ccache/ccache -M 48G
 export KBUILD_BUILD_USER=android
 export KBUILD_BUILD_HOST=localhost
 
-# start build
+#start build
+if [ "$TESTKEY" = false ] ; then
+  export OWN_KEYS_DIR=~/.android-certs
+
+  # We need symlinks to fake the existence of a testkey
+  # for the selinux build process
+  if [ ! -e $OWN_KEYS_DIR/testkey.pk8 ] ; then
+    ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
+    echo "Symlink testkey.pk8 created"
+  fi
+  if [ ! -e $OWN_KEYS_DIR/testkey.x509.pem ] ; then
+    ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
+    echo "Symlink testkey.x509.pem created"
+  fi
+fi
 brunch oneplus3
 
--- patches_apply.sh
+++ patches_apply.sh
@@ -10,6 +10,11 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
+cd vendor/lineage
+echo "Patching $PWD (build with own signing keys)"
+patch -p1 < $THISDIR/patch_002_vendor_lineage.patch
+cd $TOPDIR
+
 cd $THISDIR
 
 
