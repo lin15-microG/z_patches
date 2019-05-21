f2a095754a7a2fead873f16630b06c85522ca806
mse1969 <mse1969@posteo.de>
2019-05-22 00:02:32 +0200

    Patch device/sony/common to fix Magisk 19+ recovery issue
    Change-Id: I7a063bf5ff462ac61e78e8ef5f5cf9e33505a393

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,6 +10,11 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
+cd device/sony/common-caf
+echo "Patching $PWD (Fix Magisk 19+ recovery issue)"
+patch -p1 < $THISDIR/patch_004_device-sony-common.patch
+cd $TOPDIR
+
 cd $THISDIR
 
 
