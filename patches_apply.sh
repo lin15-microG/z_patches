dc0a7efe8d3621a467141042e375afffa338cf4e
MSe1969 <mse1969@posteo.de>
2020-02-07 15:44:57 +0100

    Remove TWRP patch (repo has been removed from build tree)
    Change-Id: Ic2b32a421bea23d1f1da7f4dc2bdd6aa6543c536

--- patches_apply.sh
+++ patches_apply.sh
@@ -15,11 +15,6 @@ echo "Patching $PWD (build with own signing keys)"
 patch -p1 < $THISDIR/patch_002_vendor_lineage.patch
 cd $TOPDIR
 
-cd bootable/recovery-twrp
-echo "Patching $PWD (work-around for 'struct epoll_event' build error)"
-patch -p1 < $THISDIR/patch_003_bootable_recovery-twrp.patch
-cd $TOPDIR
-
 cd $THISDIR
 
 
