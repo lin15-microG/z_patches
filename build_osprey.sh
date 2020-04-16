c5673b22da0c67cc4987213703eab4579e0d4e4e
MSe1969 <mse1969@posteo.de>
2020-04-16 15:49:19 +0200

    Add Osprey device
    Change-Id: I71cf03cc093504be8710cb382d9de1ddf8af7247

---
+++ build_osprey.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Check parameters
+case "$1" in
+  test) TESTKEY=true
+    ;;
+  sign) TESTKEY=false
+    ;;
+  *) echo "usage: build_osprey test|sign"
+     echo "-----------------------------------------------------"
+     echo "test - build with testkeys (insecure, but compatible)"
+     echo "sign - create a signed build"
+     exit
+    ;;   
+esac
+
+# Initiate environment
+source build/envsetup.sh
+
+# CCache
+# ------
+# uncomment the below line to set own cache 
+# directory (default is ~/.ccache)
+export USE_CCACHE=1
+#export CCACHE_DIR=~/android/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 48G
+
+# un-comment below line, if you want to build with root baked in
+# export WITH_SU=true
+
+# Normalize build metadata
+export KBUILD_BUILD_USER=android
+export KBUILD_BUILD_HOST=localhost
+
+if [ "$TESTKEY" = false ] ; then
+  export OWN_KEYS_DIR=~/.android-certs
+  export RELEASE_TYPE=UNOFFICIAL-signed
+
+  # We need symlinks to fake the existence of a testkey 
+  # for the selinux build process
+  if [ ! -e $OWN_KEYS_DIR/testkey.pk8 ] ; then
+    ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
+    echo "Symlink testkey.pk8 created"
+  fi
+  if [ ! -e $OWN_KEYS_DIR/testkey.x509.pem ] ; then
+    ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
+    echo "Symlink testkey.x509.pem created"
+  fi
+fi
+brunch osprey
+
